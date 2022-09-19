cc95da3cc0ac44f8e31853497d92a6181cfe7781
Prakash8782 <prakash8782@gmail.com>
2022-09-19 15:31:29 +0530

    pop

---
+++ pop/main.js
@@ -0,0 +1,7 @@
+let colors=["Red","Blue","Green","Yellow","Pink"];
+console.log("colors : " , colors);
+console.log("length of an array : "+colors.length);
+console.log("method : array.pop()");
+let a=colors.pop("Red");
+console.log("change in array : ",a);
+console.log("return : length of array after pop(); is "+colors.length);
